91c10fa6b0ccbbfafaa5602c9dae44377739cc30
Manik <tonysins43@gmail.com>
2023-08-15 23:12:58 +0600

    setup

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 3500;
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// import 
+const userRoutes = require('./routes/user')
+
+app.get("/",(req,res)=>{
+    res.send("Nexus Bank in Running")
+})
+app.use(userRoutes)
+
+app.listen(port,()=>{
+    console.log(`Nexus bank is running now in port:${port}`)
+})
---
+++ routes/user.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+
+router.get("/addUser",(req,res)=>{
+    res.send("User Added Successfully")
+})
+// router.post("/users", async (req, res) => {
+//     const user = req.body;
+//     const query = { email: user.email };
+//     const existingUser = await userCollection.findOne(query);
+//     if (existingUser) {
+//       return res.send({ message: "user already exits" });
+//     }
+//     const result = await userCollection.insertOne(user);
+//     res.send(result);
+//   });
+
+
+module.exports = router;
