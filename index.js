a23247d2e21b60084bc26e89b57071c51fbff895
Shuvo Deb <shuvodeb17014@gmail.com>
2023-08-18 16:32:18 +0600

    install morgan and create users api

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 require("dotenv").config();
 const app = express();
+var morgan = require('morgan')
 const port = process.env.PORT || 5000;
+
+
 // middleware
 app.use(cors());
+app.use(morgan('dev'))
 app.use(express.json());
 
 // import nice 
@@ -16,48 +20,65 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        // await client.connect();
 
-    const employeeCollection = client.db("nexusBankDB").collection("employees")
-    const loanCollection = client.db("nexusBankDB").collection("loans")
+        const usersCollection = client.db("nexusBankDB").collection("users")
+        const employeeCollection = client.db("nexusBankDB").collection("employees")
+        const loanCollection = client.db("nexusBankDB").collection("loans")
 
-    app.get('/employees', async(req,res)=>{
-      const result = await employeeCollection.find().toArray();
-      res.send(result)
-  })
+        app.get('/employees', async (req, res) => {
+            const result = await employeeCollection.find().toArray();
+            res.send(result)
+        })
 
-// add all loan request that creates the user : by default it is pending
-app.post('/apply-loan', async (req, res) => {
-  try {
-      const data = req.body;
-      const result = await loanCollection.insertOne(data);
-      res.status(200).json(result);
-  } catch (error) {
-      console.error('Error submitting loan application:', error);
-      res.status(500).json({ message: 'Loan application submission failed' });
-  }
-});
+        // add all loan request that creates the user : by default it is pending
+        app.post('/apply-loan', async (req, res) => {
+            try {
+                const data = req.body;
+                const result = await loanCollection.insertOne(data);
+                res.status(200).json(result);
+            } catch (error) {
+                console.error('Error submitting loan application:', error);
+                res.status(500).json({ message: 'Loan application submission failed' });
+            }
+        });
+
+        // users
+        app.get("/addUser", async (req,res)=>{
+            const result = await usersCollection.find().toArray();
+            res.send(result);
+        })
+        app.post("/users", async (req, res) => {
+            const user = req.body;
+            const query = { email: user.email };
+            const existingUser = await usersCollection.findOne(query);
+            if (existingUser) {
+              return res.send({ message: "user already exits" });
+            }
+            const result = await usersCollection.insertOne(user);
+            res.send(result);
+          });
 
 
-    // app.use(userRoutes)
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+        // app.use(userRoutes)
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
@@ -76,12 +97,11 @@ run().catch(console.dir);
 
 // end ------------
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.send("Nexus Bank in Running")
 })
 
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`Nexus bank is running now in port:${port}`)
 })
-
