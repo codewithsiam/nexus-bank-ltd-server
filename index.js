92736958c270a9ef0029a0a77fb78e98c49e339b
rukshanarupu <phar.rupu@gmail.com>
2023-08-18 07:11:00 +0600

    first commit

--- index.js
+++ index.js
@@ -12,8 +12,6 @@ app.use(express.json());
 // const userRoutes = require('./routes/user')
 
 
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.13jglcb.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
