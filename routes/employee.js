cba2da5efd30027f6157958fd41fa00a8d4077ca
Manik <tonysins43@gmail.com>
2023-08-26 11:37:20 +0600

    implement search and filter in employees page

--- routes/employee.js
+++ routes/employee.js
@@ -1,11 +1,11 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // Import MongoDB connection and employee collection
-const { mongoClient, employeeCollection } = require('../index');
+const { mongoClient, employeeCollection } = require("../index");
 
 // Define employee routes
-router.get('/employees', async (req, res) => {
+router.get("/employees", async (req, res) => {
   try {
     const result = await employeeCollection.find().toArray();
     res.send(result);
@@ -14,5 +14,29 @@ router.get('/employees', async (req, res) => {
     res.status(500).send("Internal Server Error");
   }
 });
+
+// employee search
+router.get("/employees/:searchItem", async (req, res) => {
+  const searchItem = req.params.searchItem;
+  if (!searchItem) {
+    return res.status(400).json({ error: "name parameter is required" });
+  }
+  const query = {
+    $or: [{ name: { $regex: searchItem, $options: "i" } }],
+  };
+  const result = await employeeCollection.find(query).toArray();
+  res.send(result);
+});
+
+// employee filter by designation 
+router.get("/employeess/:filterItem",async(req,res)=>{
+  const designation = req.params.filterItem;
+  if(!designation){
+    return res.status(400).json({error:"designation is required"})
+  }
+  const query = {designation:designation};
+  const result = await employeeCollection.find(query).toArray();
+  res.send(result)
+})
 
 module.exports = router;
