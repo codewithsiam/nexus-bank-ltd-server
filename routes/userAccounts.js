54fa862b66f0a45a6d1a80504f18d6d8c08a130e
Manik <tonysins43@gmail.com>
2023-08-29 18:10:39 +0600

    implement approve and denied account and add account number

--- routes/userAccounts.js
+++ routes/userAccounts.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { userAccountCollection } = require("..");
+const { ObjectId } = require("mongodb");
 const router = express.Router();
 
 /// store account --------------
@@ -9,10 +10,44 @@ router.post("/add-account", async (req, res) => {
   res.send(result);
 });
 
-router.get("/userAccount",async(req,res)=>{
-  const result = await userAccountCollection.find().toArray();
+// get all pending accounts ----------
+router.get("/requested-accounts",async(req,res)=>{
+  const query = {status:"pending"}
+  const result = await userAccountCollection.find(query).toArray();
   res.send(result)
 })
+
+// Function to generate a unique 10-digit account number
+async function generateUniqueAccountNumber() {
+  while (true) {
+    const randomThreeDigitNumber = Math.floor(Math.random() * 900) + 100; // Generates a random number between 100 and 999
+    const accountNumber = `1003465000${randomThreeDigitNumber}`;
+
+    const existingAccount = await userAccountCollection.findOne({ account_number: accountNumber });
+
+    if (!existingAccount) {
+      return accountNumber; // Return the unique account number
+    }
+  }
+}
+
+// handle status 
+router.patch("/status/:id",async (req, res) => {
+  const id = req.params.id;
+  const status = req.query.status;
+  const accountNumber = await generateUniqueAccountNumber();
+  // console.log(status)
+  const query = { _id: new ObjectId(id) };
+  const updateDoc = {
+    $set: {
+      status: status,
+      account_number:accountNumber
+    },
+  };
+
+  const result = await userAccountCollection.updateOne(query, updateDoc);
+  res.send(result);
+});
 
 router.get("/user-accounts", async (req, res) => {
   const { email } = req.query;
