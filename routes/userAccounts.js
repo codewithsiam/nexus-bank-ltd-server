d5d64086faaf89ee23966fa526b0e52189538782
Abu Sayed Al Siam <abusayedalsiam2023@gmail.com>
2023-08-29 10:44:36 +0600

    meney transfer api and function completed

--- routes/userAccounts.js
+++ routes/userAccounts.js
@@ -8,4 +8,12 @@ router.post("/add-account", async (req, res) => {
   res.send(result);
 });
 
+router.get("/user-accounts", async (req, res) => {
+  const { email } = req.query;
+  const filter = { email: email };
+  const result = await userAccountCollection.findOne(filter);
+  res.send(result);
+});
+
+
 module.exports = router;
