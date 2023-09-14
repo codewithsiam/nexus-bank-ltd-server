d89281492d9da1083ba48bf15d94b7cc408bce12
Abu Sayed Al Siam <abusayedalsiam2023@gmail.com>
2023-09-14 18:57:05 +0600

    ssl commerz payment gateway

--- routes/payments.js
+++ routes/payments.js
@@ -5,6 +5,7 @@ const router = express.Router();
 const stripe = require("stripe")(process.env.PAYMENT_SECRET_KEY);
 const SSLCommerzPayment = require("sslcommerz-lts");
 const { ObjectId } = require('mongodb');
+const { sendEmail } = require('../Modules/emailSend');
 const store_id = "siam647cd3e682e6d";
 const store_passwd = "siam647cd3e682e6d@ssl";
 const is_live = false;
@@ -65,105 +66,131 @@ router.post("/payments", async (req, res) => {
 });
 
 // ssl commerz 
-// router.post("/ssl-payment", async (req, res) => {
-//     const { transferToAccount, transferType, transferAmount, reason } = req.body;
+router.post("/ssl-payment", async (req, res) => {
+    const { transferToAccount, transferType, transferAmount, reason } = req.body;
 
 
 
-//     // unique transaction id 
-//     const tran_id = new ObjectId().toString();
-// const totalAmount = parseFloat(transferAmount)
+    // unique transaction id 
+    const tran_id = new ObjectId().toString();
+    const totalAmount = parseFloat(transferAmount)
 
-// const senderAccount = transferToAccount;
-//     // ssl info
-//     const data = {
-//         total_amount: totalAmount,
-//         currency: "BDT",
-//         tran_id: tran_id,
-//         transferType,
-//         reason,
-//         status: 'pending',
-//         success_url: `https://nexus-cc1a9.web.app/payment-status/success/${tran_id}`,
-//         fail_url: "https://nexus-cc1a9.web.app/payment-status/failed/no_transaction",
-//         cancel_url: "https://nexus-cc1a9.web.app/payment-status/canceled/no_transaction",
-//         ipn_url: "https://nexus-cc1a9.web.app/ipn",
-//         shipping_method: "Courier",
-//         product_name: "Computer.",
-//         product_category: "Electronic",
-//         product_profile: "general",
-//         cus_name: "Customer Name",
-//         cus_email: "customer@example.com",
-//         cus_add1: "Dhaka",
-//         cus_add2: "Dhaka",
-//         cus_city: "Dhaka",
-//         cus_state: "Dhaka",
-//         cus_postcode: "1000",
-//         cus_country: "Bangladesh",
-//         cus_phone: "01711111111",
-//         cus_fax: "01711111111",
-//         ship_name: "Customer Name",
-//         ship_add1: "Dhaka",
-//         ship_add2: "Dhaka",
-//         ship_city: "Dhaka",
-//         ship_state: "Dhaka",
-//         ship_postcode: 1000,
-//         ship_country: "Bangladesh",
-//     };
-
+    // ssl info
+    const data = {
+        total_amount: totalAmount,
+        currency: "BDT",
+        tran_id: tran_id,
+        transferType,
+        reason,
+        status: 'pending',
+        success_url: `https://nexus-cc1a9.web.app/payment-status/success/${tran_id}`,
+        fail_url: "https://nexus-cc1a9.web.app/payment-status/failed/no_transaction",
+        cancel_url: "https://nexus-cc1a9.web.app/payment-status/canceled/no_transaction",
+        ipn_url: "https://nexus-cc1a9.web.app/ipn",
+        shipping_method: "Courier",
+        product_name: "Computer.",
+        product_category: "Electronic",
+        product_profile: "general",
+        cus_name: "Customer Name",
+        cus_email: "customer@example.com",
+        cus_add1: "Dhaka",
+        cus_add2: "Dhaka",
+        cus_city: "Dhaka",
+        cus_state: "Dhaka",
+        cus_postcode: "1000",
+        cus_country: "Bangladesh",
+        cus_phone: "01711111111",
+        cus_fax: "01711111111",
+        ship_name: "Customer Name",
+        ship_add1: "Dhaka",
+        ship_add2: "Dhaka",
+        ship_city: "Dhaka",
+        ship_state: "Dhaka",
+        ship_postcode: 1000,
+        ship_country: "Bangladesh",
+    };
 
 
-//     await paymentCollection.insertOne(data);
 
-//     const sslcz = new SSLCommerzPayment(store_id, store_passwd, is_live)
-//     sslcz.init(data).then(apiResponse => {
+    await paymentCollection.insertOne(data);
 
-//         // Redirect the user to payment gateway
-//         let GatewayPageURL = apiResponse.GatewayPageURL
-//         res.send({ url: GatewayPageURL })
-//         console.log('Redirecting to: ', GatewayPageURL)
+    const sslcz = new SSLCommerzPayment(store_id, store_passwd, is_live)
+    sslcz.init(data).then(apiResponse => {
 
-//     });
+        // Redirect the user to payment gateway
+        let GatewayPageURL = apiResponse.GatewayPageURL
+        res.send({ url: GatewayPageURL })
+        console.log('Redirecting to: ', GatewayPageURL)
 
-//     const paymentConfirm = 
-//     if (apiResponse.status == "SUCCESS") {
-//         const updateDoc = {
-//             $set: { status: "Completed" },
-//         };
-
-//         const filter = { tran_id: tran_id }
-//         const result = paymentCollection.updateOne(filter, updateDoc);
-
-//         const existingAccount = userAccountCollection.findOne({ accountNumber: senderAccount });
+    });
 
 
-//         console.log("account" , existingAccount)
 
-//         if (existingAccount) {
-//             const currentBalance = existingAccount.balance;
-//             console.log("current balance", currentBalance);
-            
-//             const transferAmount = totalAmount;
-//             const newBalance = currentBalance + transferAmount;
-//             console.log("new balance", newBalance);
+    const updateDoc = {
+        $set: { status: "Completed" },
+    };
 
-//             userAccountCollection.updateOne(
-//                 { accountNumber: senderAccount },
-//                 { $set: { balance: parseFloat(newBalance) } }
-//             );
-//         } else {
-//             console.error('User not found');
-//         }
+    const filter = { tran_id: tran_id }
+    const result = paymentCollection.updateOne(filter, updateDoc);
+
+    const existingAccount = await userAccountCollection.findOne({ accountNumber: transferToAccount });
 
 
-//     } else {
-//         const updateDoc = {
-//             $set: { status: "Failed" },
-//         };
+    console.log("account", existingAccount)
 
-//         const filter = { tran_id: tran_id }
-//         const result = paymentCollection.updateOne(filter, updateDoc);
-//     }
-// });
+    if (existingAccount) {
+        const currentBalance = existingAccount.balance;
+        console.log("current balance", currentBalance);
+
+        const transferAmount = totalAmount;
+        const newBalance = currentBalance + transferAmount;
+        console.log("new balance", newBalance);
+
+        userAccountCollection.updateOne(
+            { accountNumber: transferToAccount },
+            { $set: { balance: parseFloat(newBalance) } }
+        );
+
+        const userAccount = await usersCollection.findOne({ nid_card_number: existingAccount.nid_card_number })
+        console.log("find user", userAccount)
+
+        const time = new Date().toISOString();
+        const senderSubject = `You Added Money To Your ACCOUNT: ${transferToAccount} With SSL Commerz`;
+        const senderHtmlText = `
+        <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Money Sent</title>
+</head>
+<body>
+    <h1>Money Sent Successfully</h1>
+    <p>Hello ${userAccount.username},</p>
+    <p>You have successfully add money in your account: ${transferToAccount} with the following details:</p>
+    
+    <ul>
+        <li>Amount: ${parseFloat(totalAmount)}</li>
+        <li>Date: ${time} </li>
+        <li>Transaction ID: ${tran_id}</li>
+        <li>To Account:  ${transferToAccount}</li>
+        
+    </ul>
+
+    <p>Thank you for using our services.</p>
+
+    <p>Best regards,</p>
+    <p>Nexus Bank LTD</p>
+</body>
+</html>
+
+        `
+        // sendEmail(existingAccount.email, senderSubject, senderHtmlText)
+    } else {
+        console.error('User not found');
+    }
+
+});
 
 
 router.post('/payment-status/:tranId', async (req, res) => {
