21e4848b47f3817b3006ab803b4d777365182522
Shuvo Deb <shuvodeb17014@gmail.com>
2023-09-17 00:54:38 +0600

    d

--- routes/customer.js
+++ routes/customer.js
@@ -1,17 +1,25 @@
-const express = require('express');
-const { customerCollection } = require('../index');
+const express = require("express");
+const { ObjectId } = require("mongodb");
+const { customerCollection } = require("../index");
 const router = express.Router();
 
-router.post('/support-customers', async (req, res) => {
-    const body = req.body;
-    const result = await customerCollection.insertOne(body);
-    res.send(result)
-})
+router.post("/support-customers", async (req, res) => {
+  const body = req.body;
+  const result = await customerCollection.insertOne(body);
+  res.send(result);
+});
 
-router.get('/support-customers', async (req, res) => {
-    const result = await customerCollection.find({}).toArray()
-    res.send(result)
-})
+router.get("/support-customers", async (req, res) => {
+  const result = await customerCollection.find({}).toArray();
+  res.send(result);
+});
 
+router.get("/support-customer/:id", async (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  const query = { _id: new ObjectId(id) };
+  const result = await customerCollection.findOne(query)
+  res.send(result)
+});
 
-module.exports = router
+module.exports = router;
