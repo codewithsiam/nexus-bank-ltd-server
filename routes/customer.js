2bd7810670d2abd519150f53a08dccc094c697ac
Shuvo Deb <shuvodeb17014@gmail.com>
2023-09-17 17:20:53 +0600

    create customers support api

--- routes/customer.js
+++ routes/customer.js
@@ -1,7 +1,8 @@
 const express = require("express");
-const { ObjectId } = require("mongodb");
 const { customerCollection } = require("../index");
 const router = express.Router();
+// const { ObjectId } = require("bson");
+const { ObjectId } = require("mongodb");
 
 router.post("/support-customers", async (req, res) => {
   const body = req.body;
@@ -14,12 +15,43 @@ router.get("/support-customers", async (req, res) => {
   res.send(result);
 });
 
-router.get("/support-customer/:id", async (req, res) => {
+router.get("/customer/:id", async (req, res) => {
   const id = req.params.id;
-  console.log(id);
-  const query = { _id: new ObjectId(id) };
-  const result = await customerCollection.findOne(query)
-  res.send(result)
+
+  // Check if the id is a valid ObjectId format
+  if (!/^[0-9a-fA-F]{24}$/.test(id)) {
+    return res.status(400).send("Invalid ObjectId format");
+  }
+
+  const query = { _id: new ObjectId(id) }; // Use ObjectId(id) without 'new'
+  const result = await customerCollection.findOne(query);
+
+  if (!result) {
+    return res.status(404).send("Customer not found");
+  }
+
+  res.send(result);
 });
+
+router.delete('/customer/:id', async(req,res) => {
+
+    const id = req.params.id;
+
+  // Check if the id is a valid ObjectId format
+  if (!/^[0-9a-fA-F]{24}$/.test(id)) {
+    return res.status(400).send("Invalid ObjectId format");
+  }
+
+  const query = { _id: new ObjectId(id) }; // Use ObjectId(id) without 'new'
+  const result = await customerCollection.deleteOne(query);
+
+  if (!result) {
+    return res.status(404).send("Customer not found");
+  }
+
+  res.send(result);
+})
+
+
 
 module.exports = router;
